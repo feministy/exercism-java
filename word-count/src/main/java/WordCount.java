68deeb2975e58ebbac933c7cdb12b20e396ed258
Liz Abinante <labinante@newrelic.com>
2016-05-04 17:05:38 -0700

    :fist: code compiles

--- word-count/src/main/java/WordCount.java
+++ word-count/src/main/java/WordCount.java
@@ -1,2 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordCount {
+    public WordCount() {}
+
+    public Map phrase(String word) {
+        return new HashMap<String, Integer>();
+    }
 }
